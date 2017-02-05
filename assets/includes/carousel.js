c0904ca3a9426cbad793b6c5f3d4c41fbcdefbb0
Ilaria Varriale <ilariasvarriale@gmail.com>
2017-02-04 16:46:25 -0800

    Sets carousel attributes in js object

---
+++ assets/includes/carousel.js
@@ -0,0 +1,10 @@
+var carouselAttributes = {
+  autoPlay: true,
+  currentPanel: 1,
+  totalPanels: 0,
+  timePassed: 0,
+  timeToChange: 80, // Will be multiplied to 4 seconds
+  duration: 1250, // 1.25 seconds
+  inTransition: false,
+  panelContent: Array
+};
