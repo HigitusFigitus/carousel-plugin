bf16d6c4f44fc1728e741716da0e9d55c2ba4f7f
Ilaria Varriale <ilariasvarriale@gmail.com>
2017-02-04 16:51:48 -0800

    Adds document ready function

--- assets/includes/carousel.js
+++ assets/includes/carousel.js
@@ -3,8 +3,21 @@ var carouselAttributes = {
   currentPanel: 1,
   totalPanels: 0,
   timePassed: 0,
-  timeToChange: 80, // Will be multiplied to 4 seconds
+  timeToChange: 80, // Will be multiplied to 8 seconds
   duration: 1250, // 1.25 seconds
   inTransition: false,
   panelContent: Array
 };
+
+$(document).ready(function(){
+  gatherCarouselData();
+});
+
+function gatherCarouselData(){
+  $('.carousel-data .carousel-panel').each(function(index){
+    carouselAttributes.totalPanels = index + 1;
+    var panel_image = $(this).attr('data-image') + '.jpg';
+    carouselAttributes.panelContent[index] =
+    '<div class="carousel-panel" style="background-image:url('+ panel_image +');"></div>';
+  });
+}
